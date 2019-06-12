afe0c1683e73d349a84b87bc1c0c3855fdd21806
Joomy Korkut <joomy@cattheory.com>
2019-06-11 22:32:10 -0400

    Reformat elisp

--- elisp/editor.el
+++ elisp/editor.el
@@ -14,14 +14,11 @@
     ('Editor\.move_down         (next-line) "tt")
     ('Editor\.move_beginning    (move-beginning-of-line) "tt")
     ('Editor\.move_end          (move-end) "tt")
-    (l                          (message "Unrecognized action") nil)
-  ))
+    (l                          (message "Unrecognized action") nil)))
 
 (defun parse-response (s)
   (let*
-    (
-     (untail (substring s 0 (string-match-p (regexp-quote ": Editor.edit unit") s)))
-    )
+      ((untail (substring s 0 (string-match-p (regexp-quote ": Editor.edit unit") s))))
     (pcase (read-from-string untail)
       (`(= . ,m)
        (let* ((uneq (substring untail (+ m 1)))
@@ -30,26 +27,19 @@
                           uneq))
               (paren-necessary (if (not (string-match-p (regexp-quote "Editor.bind") uneq))
                                    parenned uneq)))
-          (pcase (read-from-string paren-necessary)
-            (`(Editor\.bind . ,n)
-              (pcase (read-from-string (substring untail (+ m n 1)))
-                (`(,act . ,p) (run-edit (concat (substring untail (+ m n p 1)) " " (run-action act))))
-              )
-            )
-            (`(,act . ,m) (run-action act))
-            (l (message "Error: Expecting either a bind or an action."))
-          )
-       )
-      )
-      (l (message "Error: Expecting = in the beginning of the output."))
-    )
-  )
-)
+         (pcase (read-from-string paren-necessary)
+           (`(Editor\.bind . ,n)
+            (pcase (read-from-string (substring untail (+ m n 1)))
+              (`(,act . ,p)
+                (run-edit (concat (substring untail (+ m n p 1)) " " (run-action act))))))
+           (`(,act . ,m) (run-action act))
+           (l (message "Error: Expecting either a bind or an action.")))))
+      (l (message "Error: Expecting = in the beginning of the output.")))))
 
 (defun run-edit (s)
   (let*
-    ((res (proof-shell-invisible-cmd-get-result
-           (concat "Eval cbn in (Editor.right_assoc (" s "))."))))
+      ((res (proof-shell-invisible-cmd-get-result
+             (concat "Eval cbn in (Editor.right_assoc (" s "))."))))
     (parse-response res)))
 
 (defun run (s) (run-edit s))
